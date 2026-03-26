c268944bf7bc7fa01003e863dfc8846f22d57007
aurorazurary <aurorazurary@gmail.com>
2026-03-26 21:44:21 +0800

    Update logic to include service

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -17,6 +17,7 @@ import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Person;
 import seedu.address.model.pet.Pet;
+import seedu.address.model.service.Service;
 import seedu.address.model.session.Session;
 import seedu.address.storage.Storage;
 
@@ -76,6 +77,11 @@ public class LogicManager implements Logic {
     @Override
     public ObservableList<Pet> getFilteredPetList() {
         return model.getFilteredPetList();
+    }
+
+    @Override
+    public ObservableList<Service> getServiceList() {
+        return model.getServiceList();
     }
 
     @Override
